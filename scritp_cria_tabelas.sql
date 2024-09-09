7fe174ddd9cae2a7986378109ba90d8f79f24e91
acostaamanda <144863820+acostaamanda@users.noreply.github.com>
2024-09-09 18:20:58 -0300

    Inclusão das FK

--- scritp_cria_tabelas.sql
+++ scritp_cria_tabelas.sql
@@ -1,6 +1,6 @@
 Create Table t_procedimento
 (
-	id_procedimento		int identity,
+	id_procedimento		int identity primary key,
 	id_pessoa			int,
 	id_especialidade	int,
 	nm_procedimento		varchar(200),
@@ -9,23 +9,24 @@ Create Table t_procedimento
 	nr_senha			varchar(50)
 )
 
+
 Create Table t_especialidade
 (
-	id_especialidade	int identity,
+	id_especialidade	int identity primary key,
 	cd_especialidade	int,
 	nm_especialidade	varchar(200)
 )
 
 Create Table t_tipo_pessoa
 (
-	id_tipo_pessoa		int identity,
+	id_tipo_pessoa		int identity primary key,
 	cd_tipo_pessoa		int,
-	nm_tipo_pessoa		varchar(50)
+	nm_tipo_pessoa		varchar(50),
 )
 
 Create Table t_pessoas
 (
-	id_pessoa			int identity,
+	id_pessoa			int identity primary key,
 	id_tipo_pessoa		int,
 	id_especialidade	int,
 	nr_telefone			varchar(50),
@@ -34,4 +35,8 @@ Create Table t_pessoas
 	
 )
 
+
+ALTER TABLE t_pessoas add CONSTRAINT [FK_t_pessoas_x_t_tipo_pessoa(id_tipo_pessoa)] FOREIGN KEY ([id_tipo_pessoa]) REFERENCES [dbo].[t_tipo_pessoa] ([id_tipo_pessoa])
+ALTER TABLE t_procedimento add CONSTRAINT [FK_t_procedimento_x_t_pessoas(id_pessoa)] FOREIGN KEY ([id_pessoa]) REFERENCES [dbo].[t_pessoas] ([id_pessoa])
+ALTER TABLE t_procedimento add CONSTRAINT [FK_t_procedimento_x_t_especialidade(id_especialidade)] FOREIGN KEY ([id_especialidade]) REFERENCES [dbo].[t_especialidade] ([id_especialidade])
 
